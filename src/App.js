d883d6561ad79d20a56b1f5c5c4aac1ec632091a
balbaal <hai.balbaal@gmail.com>
2019-02-09 13:53:29 +0700

    equals warning

--- src/App.js
+++ src/App.js
@@ -22,15 +22,15 @@ class App extends Component {
   }
 
   notify = (type, desc) => {
-    if(type == 1){
+    if(type === 1){
       toast.success(desc, {
         position: toast.POSITION.RIGHT
       });
-    }else if (type == 0){
+    }else if (type === 0){
       toast.error(desc, {
         position: toast.POSITION.RIGHT
       });
-    }else if (type == 2){
+    }else if (type === 2){
       toast.warn(desc, {
         position: toast.POSITION.RIGHT
       });
